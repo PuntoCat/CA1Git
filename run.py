4d8512738ce6bfc79c037e1d334932e18f21ac44
PuntoCat <72riki@gmail.com>
2022-08-13 19:25:21 +0100

    Rendering HTML pages

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+#Database environment variables
+app.config.update(
+
+    SECRET_KEY='topsecret',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:Hypnosis321T!@localhost/catalog_db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+#Allows Flask to recognes that the DB exists.
+db = SQLAlchemy(app)
 
 @app.route('/temp')
 def using_template():
@@ -18,7 +31,23 @@ def top_movies():
                            movies=movie_list,
                            name='Harry')
 
+# PUBLICATION TABLE
+class Publication(db.Model):
+    __tablename__ = 'publication'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return 'The id is {}, Name is is {}'.format(self.id, self.name)
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
 
 
