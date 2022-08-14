78283d9ef4405f8a69f99108ae76d22fb5f7c510
PuntoCat <72riki@gmail.com>
2022-08-14 14:37:30 +0100

    Rendering HTML pages

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config.update(
 
     SECRET_KEY='topsecret',
@@ -61,7 +61,7 @@ def using_templates():
     return render_template('hello.html')
 
 
-# JINJA2 TEMPLATES-1
+# JINJA TEMPLATES 1
 @app.route('/watch')
 def top_movies():
     movie_list = ['autopsy of jane doe',
@@ -76,7 +76,7 @@ def top_movies():
                            name='Harry')
 
 
-# JINJA2 TEMPLATES-2
+# JINJA TEMPLATES 2
 @app.route('/tables')
 def movies_plus():
     movies_dict = {'autopsy of jane doe': 02.14,
@@ -127,12 +127,41 @@ class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return 'The id is {}, Name is is {}'.format(self.id, self.name)
+        return 'The Publisher is {}'.format(self.name)
+
+
+# BOOKS TABLE
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # ESTABLISH RELATIONSHIP
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
+
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return '{} by {}'.format(self.title, self.author)
 
 
 if __name__ == '__main__':
